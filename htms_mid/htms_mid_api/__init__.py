9af33d4498a46e06624ec28776eaea4a98b26155
A A <arslan.django@mail.ru>
2022-08-10 12:10:38 +0200

    Add files via upload

--- htms_mid/htms_mid_api/__init__.py
+++ htms_mid/htms_mid_api/__init__.py
@@ -1,15 +1,16 @@
 
 __title__ = 'htms_mid_api'
-__version__ = '2.3.1'
+__version__ = '3.1.0'
 __author__ = 'Arslan Aliev'
 __license__ = 'Apache License, Version 2.0'
-__copyright__ = 'Copyright (c) 2018-2021 Arslan S. Aliev'
+__copyright__ = 'Copyright (c) 2018-2022 Arslan S. Aliev'
 
 from .htdb 	import (
 	Table,
 	HTdb, 
 	Links_array,
-	Links_tree
+	Links_tree,
+    Weights_tree
 )
 from .htms_par_middle 	import (
 	DEBUG_DELETE_ROW_1,
